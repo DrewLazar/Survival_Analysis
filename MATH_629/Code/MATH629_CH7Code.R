2f08776ff42546b9d81a6675d52b8e0853200672
Drew <drewlazar@yahoo.com>
2021-11-23 21:16:36 -0500

    Chapter 7 stuff

--- MATH_629/Code/MATH629_CH7Code.R
+++ MATH_629/Code/MATH629_CH7Code.R
@@ -6,6 +6,9 @@ library(survival)
 #load the R Survival package 
 #Set working directory 
 setwd("C:/GitStuff/Survival_Analysis/MATH_629/Data")
-#load Addicts data 
-load("addicts.rda")
-modpar1=survreg(Surv(addicts$survt,addicts$status)~prison+dose+clinic,data=addicts,dist="exponential")
+#load Remission data 
+load("Remission.rda")
+#Create a Survival object 
+Y<-Surv(Remission$survt,Remission$status)
+modpar1=survreg(Y~TR,data=Remission,dist="exponential")
+            
